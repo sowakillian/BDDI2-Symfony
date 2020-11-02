e2c8a6c42aade75103a69eaf95ea173ea4b3a4e6
Sowa Killian <sowakillianpro@gmail.com>
2020-11-02 11:18:29 +0100

    first commit

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/FirstController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class FirstController
+{
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
